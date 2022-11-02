357241a9a8bcbe0008f9de9fa546ea5ecd9a7e55
Davynyam <85718151+Davynyam@users.noreply.github.com>
2022-11-02 17:10:05 +0800

    Update Payment.java
    edited the is3D() to be from Show, as it is no longer under Movie.
    changed the date = show.getShowDate() to show.getShowTime(), as the show class only has getShowTime().
    Initilialised price in line 25 as price = base_price

--- model/Payment.java
+++ model/Payment.java
@@ -22,45 +22,45 @@ public class Payment {
 
     public double calPrice(Show show, String mType)
     {
-        double price;
+        double price = base_price;
         Movie movie = show.getMovie();
-        Date date = show.getShowDate();
+        Date date = show.getShowTime();
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         int day = calendar.get(Calendar.DAY_OF_WEEK);
-        if (mType == "SENIOR" && day >= 1 && day <= 5 && date.getHours() < 18 && !movie.is3D())
+        if (mType == "SENIOR" && day >= 1 && day <= 5 && date.getHours() < 18 && !show.is3D())
         {
         		price = 0.5 * base_price;
         }
         else if (mType == "STUDENT" && day >= 1 && day <= 5 && date.getHours() < 18)
         {	
-        		if(!movie.is3D()) price = 0.8 * base_price;
+        		if(!show.is3D()) price = 0.8 * base_price;
         		else price = 0.8 * base_price + 2;
         }
         if (day >= 1 && day <= 4)
         {
-        	if(!movie.is3D()) price = base_price;
+        	if(!show.is3D()) price = base_price;
         	else price = base_price + 2;
         }
         else if (day == 5)
         {
         	if (date.getHours() < 18)
         	{
-        		if(!movie.is3D()) price = base_price;
+        		if(!show.is3D()) price = base_price;
             	else price = base_price + 2;
         	}
         	else
         	{
-        		if(!movie.is3D()) price = base_price * 1.2;
+        		if(!show.is3D()) price = base_price * 1.2;
             	else price = base_price * 1.2 + 2;
         	}	
         }
         else if( day == 0 || day == 6)
         {
-        	if(!movie.is3D()) price = base_price * 1.2;
+        	if(!show.is3D()) price = base_price * 1.2;
         	else price = base_price * 1.2 + 2;
         }
-        if(movie.isblockbuster())
+        if(movie.isBlockbuster())
         {
         	price += 1;
         }
