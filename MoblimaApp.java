7e1913e73adb973c43f1d5abe89f0c09577fc3e7
NgYJ02 <91600220+NgYJ02@users.noreply.github.com>
2022-11-13 01:52:34 +0800

    Implement Serializable in User

--- MoblimaApp.java
+++ MoblimaApp.java
@@ -18,8 +18,8 @@ public class MoblimaApp {
 	public static void main(String[] args) {
 		MainMenuUi mainMenuUI = new MainMenuUi();
 		Database db;
-		db = DataInitialiser.main();
-		//db=Serialization.read();
+		// db = DataInitialiser.main();
+		db=Serialization.read();
 		mainMenuUI.main(db);
 		Serialization.write(db);
 	}
